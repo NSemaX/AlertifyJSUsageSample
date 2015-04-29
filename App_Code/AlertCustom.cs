0e6c75ee0fa8c12daad41bf7d9cb7716348d8b19
Sema Kudu <sema.kudu@tikle.com>
2015-04-29 16:38:17 +0300

    initial commit

---
+++ App_Code/AlertCustom.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+
+public static class AlertCustom
+{
+
+    /// <summary> 
+    /// Shows a client-side JavaScript alert in the browser. 
+    /// </summary> 
+    /// <param name="message">The message to appear in the alert.</param> 
+
+
+    public static void ShowCustom(Page page,string message)
+    {
+        // Cleans the message to allow single quotation marks 
+        string cleanMessage = message.Replace("'", "\\'");
+        string script = "<script >alertify.alert(\"" + cleanMessage + "\" ); </script>";
+        page = HttpContext.Current.CurrentHandler as Page;
+        // Checks if the handler is a Page and that the script isn't allready on the Page 
+        if (page != null && !page.ClientScript.IsClientScriptBlockRegistered("alertcustom"))
+        {
+            page.ClientScript.RegisterClientScriptBlock(typeof(AlertCustom), "alertcustom", script);
+        }
+
+    }
+    public static void ShowSuccess(Page page, string message)
+    {
+        // Cleans the message to allow single quotation marks 
+        string cleanMessage = message.Replace("'", "\\'");
+        string script = "<script >alertify.success(\"" + cleanMessage + "\" ); </script>";
+         page = HttpContext.Current.CurrentHandler as Page;
+        // Checks if the handler is a Page and that the script isn't allready on the Page 
+        if (page != null && !page.ClientScript.IsClientScriptBlockRegistered("alertcustomsuccess"))
+        {
+            page.ClientScript.RegisterClientScriptBlock(typeof(AlertCustom), "alertcustomsuccess", script);
+        }
+
+    }
+    public static void ShowError(Page page, string message)
+    {
+        // Cleans the message to allow single quotation marks 
+        string cleanMessage = message.Replace("'", "\\'");
+        string script = "<script >alertify.error(\"" + cleanMessage + "\" ); </script>";
+       page = HttpContext.Current.CurrentHandler as Page;
+        // Checks if the handler is a Page and that the script isn't allready on the Page 
+        if (page != null && !page.ClientScript.IsClientScriptBlockRegistered("alertcustomerror"))
+        {
+            page.ClientScript.RegisterClientScriptBlock(typeof(AlertCustom), "alertcustomerror", script);
+        }
+
+    }
+    public static void ShowNormal(Page page, string message)
+    {
+        // Cleans the message to allow single quotation marks 
+        string cleanMessage = message.Replace("'", "\\'");
+        string script = "<script >alertify.message(\"" + cleanMessage + "\" ); </script>";
+     page = HttpContext.Current.CurrentHandler as Page;
+        // Checks if the handler is a Page and that the script isn't allready on the Page 
+        if (page != null && !page.ClientScript.IsClientScriptBlockRegistered("alertcustomnormal"))
+        {
+            page.ClientScript.RegisterClientScriptBlock(typeof(AlertCustom), "alertcustomnormal", script);
+        }
+
+    }
+}
---
+++ Default.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+ <title>Alertify Usage</title>
+    <!-- include the script -->    
+<script src="alertifyJS/alertify.min.js"></script>
+    <!-- include the style -->
+<link rel="stylesheet" href="alertifyJS/alertify.min.css" />
+<!-- include a theme -->
+<link rel="stylesheet" href="alertifyJS/default.min.css" />
+     <script type="text/javascript">
+        function clickedButton() {
+            alert('Hello');
+        }
+    </script>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    <asp:Button runat="server" Text="Click Me" ID="hello_btn"  OnClick="hello_btn_Click" />
+
+     <button onclick="clickedButton()">Click Me 1</button>
+    </div>
+    </form>
+</body>
+</html>
